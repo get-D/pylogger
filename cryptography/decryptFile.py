dc599986ec53dcf90c610af4b1da39cf8560a792
get-D <divyatwa5@gmail.com>
2026-01-30 06:03:02 +0530

    decrypting files

---
+++ cryptography/decryptFile.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+
+key = ""
+
+e_keys = "e_key_log.txt"
+e_sys_info = "e_sysInfo.txt"
+e_clipboard_info = "e_clipboard.txt"
+
+encrypted_files = [e_key_log.txt, e_sysInfo.txt, e_clipboard.txt]
+
+count = 0
+
+for file in encrypted_files:
+
+    with open(encrypted_files[file_count], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+
+    with open(encrypted_files[file_count], 'wb') as f:
+        f.write(decrypted)
+    
+    file_count += 1
